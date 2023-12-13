46c95433ea28f77daafb4ea1f2a52f5f71cba198
“Mirza” <mirza_azmathullah@cognerium.com>
2023-12-13 15:28:39 +0530

    first commit

---
+++ lib/login_screen.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:SafeArea(
+        child:  SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                const  Spacer(),
+               const  Center(child: Text("Login",style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 40),)),
+              const  SizedBox(
+                  height: 40,
+                ),
+                Container(
+                  padding:const  EdgeInsets.symmetric(horizontal: 20),
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade300,
+                    borderRadius: BorderRadius.circular(25)
+                  ),
+                  child:TextFormField(
+
+                    decoration:const  InputDecoration(
+                      hintText: "Username",
+
+                      border: InputBorder.none,
+                      prefixIcon: Icon(Icons.person_2_outlined,color: Colors.grey,)
+                    ),
+                  ) ,
+                ),
+              const   SizedBox(height: 20,),
+                Container(
+                  padding:const  EdgeInsets.symmetric(horizontal: 20),
+                  decoration: BoxDecoration(
+                      color: Colors.grey.shade300,
+                      borderRadius: BorderRadius.circular(25)
+                  ),
+                  child:TextFormField(
+
+                    decoration:const  InputDecoration(
+                        hintText: "Password",
+
+                        border: InputBorder.none,
+                        prefixIcon: Icon(Icons.lock,color: Colors.grey,)
+                    ),
+                  ) ,
+                ),
+              const   SizedBox(height: 20,),
+                Row(
+                  children: [
+                    Checkbox(
+                      activeColor: Colors.amber,
+                        value: true, onChanged: (val){}),
+                    Text("Remember me",style: TextStyle(fontSize: 17,color: Colors.grey.shade700),)
+                  ],
+                ),
+                const   SizedBox(height: 20,),
+                Container(
+                  height: 50,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      borderRadius:BorderRadius.circular(25),
+                    gradient:const  LinearGradient(
+                      colors: [
+                         Colors.amber,
+                        Colors.pink
+                      ]
+                    ),
+
+                  ),
+                  alignment: Alignment.center,
+                  child:const  Text("LOG IN",style: TextStyle(color: Colors.white,fontSize: 20),),
+                ),
+              const   SizedBox(height: 20,),
+                Text("Forget Password?",style: TextStyle(fontSize: 17,color: Colors.grey.shade700),),
+              const  Spacer(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Not a member?",style: TextStyle(fontSize: 17,color: Colors.grey.shade700),),
+                   const  SizedBox(width: 10,),
+                   const  Text("Sign up now",style: TextStyle(fontSize: 17,color: Colors.blueAccent),)
+                  ],
+                )
+
+              ],
+            ),
+          ),
+        ),
+      )
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'login_screen.dart';
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
+  }
+}
+
